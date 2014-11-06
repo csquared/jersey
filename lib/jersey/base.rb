2427b56ed91c6e8f53bf96d0f2413f34faa7d9b6
csquared <christopher.continanza@gmail.com>
2014-11-05 22:24:49 -0800

    paste in some code

--- lib/jersey/base.rb
+++ lib/jersey/base.rb
@@ -1,13 +1,61 @@
 require 'sinatra'
 require 'sinatra/json'
 
+require 'jersey/log'
+require 'jersey/request_id'
+
+class RequestLogger
+  def initialize(app, options={})
+    @app = app
+    @logger = Jersey::LogfmtLogger.new
+  end
+
+  def call(env)
+    @request_start = Time.now
+    request = Rack::Request.new(env)
+    @logger.log(
+      at:              "start",
+      request_id:      env['REQUEST_ID'],
+      method:          request.request_method,
+      path:            request.path_info
+    )
+    status, headers, response = @app.call(env)
+    @logger.log(
+      at:              "finish",
+      method:          request.request_method,
+      path:            request.path_info,
+      status:          status,
+      'size#bytes' =>  headers['Content-Length'] || reaponse.size,
+      route_signature: env['ROUTE_SIGNATURE'],
+      request_id:      env['REQUEST_ID'],
+      elapsed:         (Time.now - @request_start).to_f
+    )
+    [status, headers, response]
+  end
+end
+
+module RouteSignature
+  def self.registered(app)
+    app.helpers do
+      def route_signature
+        env["ROUTE_SIGNATURE"]
+      end
+    end
+  end
+
+  def route(verb, path, *)
+    condition { env["ROUTE_SIGNATURE"] = path.to_s }
+    super
+  end
+end
+
 module Jersey::API
   class Base < Sinatra::Base
     include Jersey::HTTP::Errors
 
-#   register Pliny::Extensions::Instruments
- #  use Pliny::Middleware::RequestID
- #  use Pliny::Middleware::Timeout
+    register RouteSignature
+    use RequestID
+    use RequestLogger
 
     helpers Sinatra::JSON
 
---
+++ lib/jersey/log.rb
@@ -0,0 +1,74 @@
+module Jersey
+  class LogfmtLogger
+    attr_accessor :stream
+
+    def initialize(stream = $stdout, defaults = {})
+      @defaults = defaults
+      @stream = stream
+    end
+
+    def <<(data)
+      @defaults.merge!(data)
+    end
+
+    def log(data, &block)
+      log_to_stream(stream, @defaults.merge(data), &block)
+    end
+
+    private
+
+    def log_to_stream(stream, data, &block)
+      unless block
+        str = unparse(data.merge(now: Time.now))
+        stream.print(str + "\n")
+      else
+        data = data.dup
+        start = Time.now
+        log_to_stream(stream, data.merge(at: "start"))
+        begin
+          res = yield
+          log_to_stream(stream, data.merge(
+            at: "finish", elapsed: (Time.now - start).to_f))
+          res
+        rescue
+          log_to_stream(stream, data.merge(
+            at: "exception", elapsed: (Time.now - start).to_f))
+          raise
+        end
+      end
+    end
+
+    def quote_string(k, v)
+      # try to find a quote style that fits
+      if !v.include?('"')
+        %{#{k}="#{v}"}
+      elsif !v.include?("'")
+        %{#{k}='#{v}'}
+      else
+        %{#{k}="#{v.gsub(/"/, '\\"')}"}
+      end
+    end
+
+    def unparse(attrs)
+      attrs.map { |k, v| unparse_pair(k, v) }.compact.join(" ")
+    end
+
+    def unparse_pair(k, v)
+      v = v.call if v.is_a?(Proc)
+      # only quote strings if they include whitespace
+      if v == nil
+        nil
+      elsif v == true
+        k
+      elsif v.is_a?(Float)
+        "#{k}=#{format("%.3f", v)}"
+      elsif v.is_a?(String) && v =~ /\s/
+        quote_string(k, v)
+      elsif v.is_a?(Time)
+        "#{k}=#{v.iso8601}"
+      else
+        "#{k}=#{v}"
+      end
+    end
+  end
+end
